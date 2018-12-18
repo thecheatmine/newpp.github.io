238215d051820c1c6e9da45ab30b11abf2e0ef0d
thecheatmine <thecheatermail@gmail.com>
2018-12-18 14:59:44 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -2,3 +2,26 @@ class map {
 
 }
 
+class current {
+
+}
+
+class result {
+  constructor(aim, tap, read){
+    this.aim = aim;
+    this.tap = tap;
+    this.read = read;
+  }
+
+  changeAim(value){
+    this.aim += value;
+  }
+
+  changeTap(value){
+    this.tap += value;
+  }
+
+  changeRead(value){
+    this.read += value;
+  }
+}
